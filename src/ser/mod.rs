bae48a56dbffbd47ded0ab3bc123e45a637f7c45
Erik Johnston <erikj@jki.re>
2016-05-01 20:46:12 +0100

    Add encode_canonnically

--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -2,10 +2,16 @@ pub mod signatures;
 
 use indolentjson::compact::compact as compact_json;
 
+use serde::Serialize;
 use serde_json;
 
 pub fn canonicalize(bytes: &[u8]) -> serde_json::Result<Vec<u8>> {
-    let mut val: serde_json::Value = try!(serde_json::from_slice(bytes));
+    let val: serde_json::Value = try!(serde_json::from_slice(bytes));
+    encode_canonically(&val)
+}
+
+pub fn encode_canonically<S: Serialize>(st: &S) -> serde_json::Result<Vec<u8>> {
+    let mut val: serde_json::Value = serde_json::to_value(st);
 
     if let Some(obj) = val.as_object_mut() {
         obj.remove("signatures");
@@ -25,11 +31,21 @@ pub fn canonicalize(bytes: &[u8]) -> serde_json::Result<Vec<u8>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json;
 
     #[test]
     fn canonical() {
         let bytes = br#"{"old_verify_keys":{},"server_name":"jki.re","signatures":{"jki.re":{"ed25519:auto":"X2t7jN0jaJsiZWp57da9GqmQ874QFbukCMSqc5VclaB+2n4i8LPcZDkD6+fzg4tkfpSsiIDogkY4HWv1cnGhAg"}},"tls_fingerprints":[{"sha256":"Big0aXVWZ/m0oEcHddgP4hTriTEvb4Jx6592W1mB5i4"}],"valid_until_ts":1462110302047,"verify_keys":{"ed25519:auto":{"key":"Sr/Vj3FIqyQ2WjJ9fWpUXRdz6fX4oFAjKrDmu198PnI"}}}"#;
         let canonical = canonicalize(bytes).unwrap();
+
+        assert_eq!(&canonical[..], &br#"{"old_verify_keys":{},"server_name":"jki.re","tls_fingerprints":[{"sha256":"Big0aXVWZ/m0oEcHddgP4hTriTEvb4Jx6592W1mB5i4"}],"valid_until_ts":1462110302047,"verify_keys":{"ed25519:auto":{"key":"Sr/Vj3FIqyQ2WjJ9fWpUXRdz6fX4oFAjKrDmu198PnI"}}}"#[..]);
+    }
+
+    #[test]
+    fn encode_canonical() {
+        let bytes = br#"{"old_verify_keys":{},"server_name":"jki.re","signatures":{"jki.re":{"ed25519:auto":"X2t7jN0jaJsiZWp57da9GqmQ874QFbukCMSqc5VclaB+2n4i8LPcZDkD6+fzg4tkfpSsiIDogkY4HWv1cnGhAg"}},"tls_fingerprints":[{"sha256":"Big0aXVWZ/m0oEcHddgP4hTriTEvb4Jx6592W1mB5i4"}],"valid_until_ts":1462110302047,"verify_keys":{"ed25519:auto":{"key":"Sr/Vj3FIqyQ2WjJ9fWpUXRdz6fX4oFAjKrDmu198PnI"}}}"#;
+        let deser: serde_json::Value = serde_json::from_slice(bytes).unwrap();
+        let canonical = encode_canonically(&deser).unwrap();
 
         assert_eq!(&canonical[..], &br#"{"old_verify_keys":{},"server_name":"jki.re","tls_fingerprints":[{"sha256":"Big0aXVWZ/m0oEcHddgP4hTriTEvb4Jx6592W1mB5i4"}],"valid_until_ts":1462110302047,"verify_keys":{"ed25519:auto":{"key":"Sr/Vj3FIqyQ2WjJ9fWpUXRdz6fX4oFAjKrDmu198PnI"}}}"#[..]);
     }
